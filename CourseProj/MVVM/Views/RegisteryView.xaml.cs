acfafa7d6c34f74ae1b34f896f42b9cd329201f3
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-05-27 20:31:03 +0300

    some validation added, new pictures added, logo added

--- CourseProj/MVVM/Views/RegisteryView.xaml.cs
+++ CourseProj/MVVM/Views/RegisteryView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,11 +28,32 @@ namespace CourseProj.MVVM.Views
 
         private void PasswordTextBox_OnLostFocus(object sender, RoutedEventArgs e)
         {
-            if (PasswordTextBox.Password.Length < 5)
+            try
             {
-                MessageBox.Show("Слишком короткий пароль", "Предупреждение");
-                PasswordTextBox.Password = "";
+                if (PasswordTextBox.Password != null)
+                {
+                    if (PasswordTextBox.Password.Length < 5)
+                    {
+                        MessageBox.Show("Слишком короткий пароль", "Предупреждение");
+                        PasswordTextBox.Password = "";
+                    }
+                }
             }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+
+        private void MailTextBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            if (!regex.IsMatch(MailTextBox.Text))
+            {
+                MessageBox.Show("Неверный формат электронной почты", "Предупреждение");
+                MailTextBox.Text = "";
+            }
+            //return regex.IsMatch(s);
         }
     }
 }
